d8b7232a2f39029ed7aafbb00dd3607b3b2382cd
Sarthak Chavan <144462055+sarthak11234@users.noreply.github.com>
2025-09-26 19:54:51 +0530

    Update parameterized.sql

--- Cursors/parameterized.sql
+++ Cursors/parameterized.sql
@@ -1,8 +1,14 @@
 DECLARE
-   CURSOR c(p NUMBER) IS SELECT name FROM product WHERE price > p;
+   p NUMBER;  
+   CURSOR c(p NUMBER) IS 
+       SELECT name FROM product WHERE price > p;
 BEGIN
-   FOR rec IN c(500) LOOP
+   
+   p := &price_value;
+
+   FOR rec IN c(p) LOOP
       DBMS_OUTPUT.PUT_LINE(rec.name);
    END LOOP;
 END;
 /
+
